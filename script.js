c3e77a6f031ff64a0a94c689eb6806022d1300b1
ab2go <abdou.goujjani@gmail.com>
2026-09-11 15:07:33 +0100

    Make catalog search index load independently

--- script.js
+++ script.js
@@ -36,10 +36,12 @@
   };
 
   load('script-core.js')
-    .then(() => load('language.js'))
-    .then(() => load('catalog-language.js'))
-    .then(() => load('site-content-language.js'))
-    .then(() => load('catalog-search-index.js'))
+    .then(() => Promise.allSettled([
+      load('language.js'),
+      load('catalog-language.js'),
+      load('site-content-language.js'),
+      load('catalog-search-index.js')
+    ]))
     .then(() => load('site-search.js'))
     .then(() => {
       setupPrimaryNav();
