f6d7f030468776627ac92a9651abc81962f77267
ab2go <abdou.goujjani@gmail.com>
2026-09-14 13:08:20 +0100

    Finalize product expansion loading and cache version

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 (() => {
-  const VERSION = '20260914b';
+  const VERSION = '20260914c';
 
   const load = src => new Promise((resolve, reject) => {
     const script = document.createElement('script');
@@ -39,6 +39,7 @@
 
   load('script-core.js')
     .then(() => document.body.classList.contains('products-page') ? load('product-expansion.js') : null)
+    .then(() => document.body.classList.contains('products-page') ? load('product-interactions.js') : null)
     .then(() => load('language.js'))
     .then(() => load('catalog-language.js'))
     .then(() => load('site-content-language.js'))
