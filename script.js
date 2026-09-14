fcc98fb2fcf68058b1cfe3a473ad02b5b2460c79
ab2go <abdou.goujjani@gmail.com>
2026-09-14 15:22:09 +0100

    Bump shared loader cache after contact refinement

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 (() => {
-  const VERSION = '20260914d';
+  const VERSION = '20260914e';
 
   const load = src => new Promise((resolve, reject) => {
     const script = document.createElement('script');
